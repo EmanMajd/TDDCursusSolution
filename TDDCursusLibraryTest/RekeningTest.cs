9b2a87431730033c09a38ce39bbc3fcb3c67f723
Eman Kalloub <118176301+EmanMajd@users.noreply.github.com>
2023-01-24 10:19:57 +0100

    Add files via upload

---
+++ TDDCursusLibraryTest/RekeningTest.cs
@@ -0,0 +1,51 @@
+﻿using TDDCursusLibrary;
+
+
+namespace TDDCursusLibraryTest;
+
+public class RekeningTest
+{
+    // Het Saldo Van Een Nieuwe Rekening Is Nul
+    [TestMethod]
+    public void Saldo_NieuwRekening_IsNul()
+    {
+        // Arrange
+        var rekening = new Rekening();
+
+        // Act  
+
+        // Assert
+        Assert.AreEqual(decimal.Zero, rekening.Saldo);
+    }
+
+    // Het Saldo Na Een Eerste Storting Is Het Bedrag Van Die Storting
+    [TestMethod]
+    public void Saldo_NaEersteStorting_BedragVanDeStorting()
+    {
+        // Arrange
+        var rekening = new Rekening();
+        var bedrag = 2.5m;
+
+        // Act
+        rekening.Storten(bedrag);
+
+        // Assert
+        Assert.AreEqual(bedrag, rekening.Saldo);
+    }
+
+    // Het Saldo Na Twee Stortingen Is De Som Van De Bedragen Van Die Stortingen
+    [TestMethod]
+    public void Saldo_NaTweeStortingen_SomVanDeStortingen()
+    {
+
+        // Arrange
+        var rekening = new Rekening();
+
+        // Act
+        rekening.Storten(2.5m);
+        rekening.Storten(1.2m);
+
+        // Assert
+        Assert.AreEqual(3.7m, rekening.Saldo);
+    }
+}
