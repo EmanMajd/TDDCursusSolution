2c7a255a363999f27f4d104220b9a0dfeded509d
Eman Kalloub <118176301+EmanMajd@users.noreply.github.com>
2023-01-24 10:19:28 +0100

    Add files via upload

---
+++ TDDCursusLibrary/Rekening.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TDDCursusLibrary
+{
+    public class Rekening
+    {
+        private decimal saldo;
+
+        public decimal Saldo
+        {
+            get
+            {
+                return saldo;
+            }
+        }
+
+        public void Storten(decimal bedrag)
+        {
+            // throw new NotImplementedException();
+            saldo += bedrag;
+
+        }
+
+
+
+    }
+}
